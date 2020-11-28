55e1f040cd4de23510b9570fb19e6d379ed543af
Muhammad Rafly Indrawan <fliw@digitalkode.com>
2020-11-28 21:45:18 +0700

    Add MariaDB Connection Module

---
+++ Inventory/Inventory/model/DBConnect.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+using System.Data;
+namespace Inventory.model
+{
+    class DBConnect
+    {
+        public static MySqlConnection GetConnection()
+        {
+            string strConnect = "SERVER = localhost;" + "PORT = 3306;" + "UID = root;" + "PWD = ;" + "DATABASE = pemograman";
+            return new MySqlConnection(strConnect);
+        }
+    }
+}
