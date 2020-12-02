6ea7b62a71ae9637c0c579c60df8491fba0feda3
Muhammad Rafly Indrawan <fliw@digitalkode.com>
2020-12-02 22:29:06 +0700

    Tidy Up Directory

--- Inventory/Inventory/view/Login.xaml.cs
+++ Inventory/Inventory/view/Login.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -12,7 +11,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Windows;
-using System.Windows.Controls;
 using Inventory.controller;
 
 namespace Inventory.view
