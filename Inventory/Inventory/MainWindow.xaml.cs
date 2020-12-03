196cc709f1e3f6d2f57715fa68225ae94d4da619
G-Saka <Gatotsasangkataka@gmail.com>
2020-12-03 21:50:24 +0700

    MenghapusKesalahanku

---
+++ Inventory/Inventory/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace Inventory
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MySqlConnection conn;
+        public MainWindow()
+        {
+            conn = model.DBConnect.GetConnection();
+            try
+            {
+                conn.Open();
+            }
+            catch(MySqlException se)
+            {
+                MessageBox.Show("Koneksi Gagal!" + se);
+            }
+            InitializeComponent();
+            view.Window1 init = new view.Window1();
+            init.Show();
+            this.Close();
+        }
+    }
+}
